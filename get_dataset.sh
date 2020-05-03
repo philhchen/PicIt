212443763b699eb705c7c1d5055aa3ab9e6042b9
Phil Chen <phil@Phils-MacBook-Pro.local>
2020-05-03 15:36:06 -0700

    Update setup scripts

--- get_dataset.sh
+++ get_dataset.sh
@@ -3,5 +3,6 @@
 source .env/bin/activate
 
 # # Download part of the dataset and parse the dataset
+mkdir dataset
 gsutil cp 'gs://quickdraw_dataset/full/simplified/a*.ndjson' dataset
 python data/data_utils.py
